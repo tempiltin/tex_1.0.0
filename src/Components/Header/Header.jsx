60ec695a53f453e6285379f6c30e41a8e9a96900
tempiltin <temurbekshukurov0707@gmail.com>
2022-07-19 09:36:38 +0500

    upload

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
 
       <section className="swiper-slide slide swiper-slide2" >
          <div className="content">
-            <h3>eng yaxshi kurslarni shu yerda topasiz!</h3>
-            <p>Yigirma birinchi asrning eng taraqqiy etgan yo'nalishlaridan biri bu axborot va telekommunikatsiya yo'nalishidir, asosan dasturiy ta'minot va sun'iy intellekt rivojlanishi va qo'llanilishi dolzarb bo'lib qolgan.</p>
-            <a href="#!" className="btn">get started</a>
+            <h3>Narpay xizmat ko'rsatish texnikumi!</h3>
+            <p>Texnikumlarga hujjatlarni onlayn topshirish tizimi</p>
+            <a href="https://texnikum.edu.uz/auth/?token=g9ui35bL904bvt0d1rS0l9jbcNv59bdvmq317Kxr1y1r540yhyTv11Bf03702m26v6G01n43cE60i320spc2ni013aHdAeGjuh7h79j5hfik1qHEEi70Bnd3na103x2d" className="btn"> Ariza topshirish</a>
          </div>
       </section>
 
--- src/Components/Subject/Subject.jsx
+++ src/Components/Subject/Subject.jsx
@@ -1,55 +1,57 @@
 import React from 'react'
-const icon1 = "/assets/images/subject-icon-1.png"
-const icon2 = "../../assets/images/subject-icon-2.png"
-const icon3 = "../../assets/images/subject-icon-3.png"
-const icon4 = "../../assets/images/subject-icon-4.png"
-const icon5 = "../../assets/images/subject-icon-5.png"
-const icon6 = "../../assets/images/subject-icon-6.png"
+
 
 const Subject = () => {
   return (
     <section className="subjects">
 
-   <h1 className="heading">bizning mashhur mavzularimiz</h1>
+   <h1 className="heading">bizning mashhur yo'nalishlarimiz</h1>
 
    <div className="box-container">
 
       <div className="box">
-         <img src={icon1} alt=""/>
-         <h3>graphic design</h3>
-         <p>12 modul</p>
+         <h3>Kompiyuter engineering</h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli +</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
-
+      
       <div className="box">
-         <img src={icon2} alt=""/>
-         <h3>matematika</h3>
-         <p>12 modul</p>
+         <h3>Ishlab chiqarish ta'limi </h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli +</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
-
       <div className="box">
-         <img src={icon3} alt=""/>
-         <h3>teaching</h3>
-         <p>12 modules</p>
+         <h3>Tashishni tashkil etish va transportni boshqarish</h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli +</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
-
       <div className="box">
-         <img src={icon4} alt=""/>
-         <h3>development</h3>
-         <p>12 modules</p>
+         <h3>Avtomobil transportiga texnik xizmat ko'rsatish</h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli +</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
-
+      
       <div className="box">
-         <img src={icon5} alt=""/>
-         <h3>science</h3>
-         <p>12 modules</p>
+         <h3>Buxgalteriya va audit</h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli +</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
-
+      
       <div className="box">
-         <img src={icon6} alt=""/>
-         <h3>engineering</h3>
-         <p>12 modules</p>
+         <h3>Yengil sanoat</h3>
+         <p>kunduzgi ta'lim shakli +</p>
+         <p>kechki ta'lim shakli -</p>
+         <p>kunduzgi ta'lim shakli +</p>
       </div>
+      
 
+      
+ 
    </div>
 
 </section>
